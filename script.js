41b21a36eeb15e5e9b042fa6c307ab8ec0f8a174
carinarocha22 <carinabarbosarocha@gmail.com>
2026-02-17 19:54:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ let carrinho = [];
 
 function adicionarAoCarrinho(nome, preco){
   carrinho.push({nome, preco});
+  document.getElementById("contador").innerText = carrinho.length;
   alert(nome + " adicionado ao carrinho.");
 }
+
+function toggleAcessibilidade(){
+  document.body.classList.toggle("alto-contraste");
+}
+
+document.querySelectorAll("button, a, h1, h2, h3, p").forEach(el => {
+  el.addEventListener("mouseover", () => {
+    let texto = el.innerText;
+    let speech = new SpeechSynthesisUtterance(texto);
+    speech.lang = "pt-PT";
+    window.speechSynthesis.speak(speech);
+  });
+});

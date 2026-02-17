2830940f6ea018152b8601c828f9002d5b6fc8c1
carinarocha22 <carinabarbosarocha@gmail.com>
2026-02-17 22:46:16 +0000

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
 
 function adicionarCarrinho(nome, preco) {
-  carrinho.push({ nome, preco });
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
-  alert("Produto adicionado ao carrinho!");
+    carrinho.push({ nome, preco });
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+    alert("Produto adicionado ao carrinho!");
 }
 
-if (document.getElementById("carrinho-lista")) {
-  let lista = document.getElementById("carrinho-lista");
-  let total = 0;
+if (document.getElementById("lista-carrinho")) {
+    let lista = document.getElementById("lista-carrinho");
+    let total = 0;
 
-  carrinho.forEach(item => {
-    let p = document.createElement("p");
-    p.textContent = item.nome + " - " + item.preco + "€";
-    lista.appendChild(p);
-    total += item.preco;
-  });
+    carrinho.forEach(item => {
+        let p = document.createElement("p");
+        p.textContent = item.nome + " - " + item.preco + "€";
+        lista.appendChild(p);
+        total += item.preco;
+    });
 
-  document.getElementById("total").textContent = "Total: " + total + "€";
+    document.getElementById("total").textContent = "Total: " + total + "€";
 }
-

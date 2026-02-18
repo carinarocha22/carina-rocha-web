6a93efe67d4d9eace10ef3bffc4a25201e1838d3
carinarocha22 <carinabarbosarocha@gmail.com>
2026-02-18 22:13:52 +0000

    js/products.js

---
+++ js/products.js
@@ -0,0 +1,99 @@
+const products = [
+    {
+        id: 1,
+        name: "Laneige Water Sleeping Mask",
+        brand: "Laneige",
+        price: 29.90,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1608248597279-f99d160bfcbc"
+    },
+    {
+        id: 2,
+        name: "COSRX Advanced Snail 96 Essence",
+        brand: "COSRX",
+        price: 24.90,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1596462502278-27bfdc403348"
+    },
+    {
+        id: 3,
+        name: "Beauty of Joseon Glow Serum",
+        brand: "Beauty of Joseon",
+        price: 21.50,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1629198688000-71f23e745b6e"
+    },
+    {
+        id: 4,
+        name: "Innisfree Green Tea Cleanser",
+        brand: "Innisfree",
+        price: 17.90,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1585386959984-a41552262d4d"
+    },
+    {
+        id: 5,
+        name: "Shiseido Ultimune Serum",
+        brand: "Shiseido",
+        price: 74.90,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1580281657527-47a9a8c8c8cb"
+    },
+    {
+        id: 6,
+        name: "Etude House Lip Tint",
+        brand: "Etude House",
+        price: 12.90,
+        category: "Maquilhagem",
+        image: "https://images.unsplash.com/photo-1611930022073-b7a4ba5fcccd"
+    },
+    {
+        id: 7,
+        name: "Clio Kill Lash Mascara",
+        brand: "Clio",
+        price: 18.90,
+        category: "Maquilhagem",
+        image: "https://images.unsplash.com/photo-1583001809873-a128495da465"
+    },
+    {
+        id: 8,
+        name: "Missha Magic Cushion Foundation",
+        brand: "Missha",
+        price: 19.90,
+        category: "Maquilhagem",
+        image: "https://images.unsplash.com/photo-1600180758890-6c8f8a8f9b06"
+    },
+    {
+        id: 9,
+        name: "Tsubaki Premium Repair Shampoo",
+        brand: "Tsubaki",
+        price: 15.90,
+        category: "Haircare",
+        image: "https://images.unsplash.com/photo-1595433562696-79f8c95e1c5f"
+    },
+    {
+        id: 10,
+        name: "Kao Essential Hair Mask",
+        brand: "Kao",
+        price: 14.90,
+        category: "Haircare",
+        image: "https://images.unsplash.com/photo-1585238342024-78d387f4a707"
+    },
+    {
+        id: 11,
+        name: "Hada Labo Gokujyun Lotion",
+        brand: "Hada Labo",
+        price: 18.90,
+        category: "Skincare",
+        image: "https://images.unsplash.com/photo-1571781926291-c477ebfd024b"
+    },
+    {
+        id: 12,
+        name: "Canmake Cream Cheek Blush",
+        brand: "Canmake",
+        price: 11.90,
+        category: "Maquilhagem",
+        image: "https://images.unsplash.com/photo-1615634262414-6d2b49b3b94c"
+    }
+];
+
